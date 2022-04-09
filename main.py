2ac79d6ee80252ae69d7b49a5c57367c44522846
Nijgururaj Ashtagi <90544127+nijgururajofficial@users.noreply.github.com>
2022-04-09 17:33:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
-############### Our Blackjack House Rules #####################
-
-## The deck is unlimited in size. 
-## There are no jokers. 
-## The Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
-## The computer is the dealer.
 import random
-import replit
+import os
 from art import logo
 
+# selects a random card from the set of cards
 def deal_card():
   cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
   card = random.choice(cards)
   return card
 
+# return the total score
 def calculate_score(card_list):
   
   if sum(card_list) == 21 and len(card_list) == 2:
+#     returns 0 if score is 21 that is BlackJack
     return 0
 
   if 11 in card_list and sum(card_list) > 21:
@@ -28,6 +20,7 @@ def calculate_score(card_list):
     card_list.append(1)
   return sum(card_list)
 
+# compares the player's and computer's score
 def compare(user_score,computer_score):
   if user_score > 21 and computer_score > 21:
     return "You went over. You lose!"
@@ -47,16 +40,17 @@ def compare(user_score,computer_score):
   else:
     return "You lose"
 
+# main play function
 def play():
   print(logo)
   user_cards = []
   computer_cards = []
   game_over = False
-  
+#   two random cards for each player at begining
   for i in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
-
+# loop until game iis not over
   while not game_over:
     user_score = calculate_score(user_cards)
     computer_score = calculate_score(computer_cards)
@@ -81,6 +75,7 @@ def play():
   print(f"Computer's final cards: {computer_cards}, final score: {computer_score}")
   print(compare(user_score, computer_score))
 
+#   while loop to play multiple times
 while input("Do you want to play BlackJack..? Type 'y' for yes and 'n' fo no: ").lower() == 'y':
-  replit.clear()
+  os.system('clear')
   play()
